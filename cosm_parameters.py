a3e15cf469acf3a0c23e3f000811ef39c2e7bda4
FedeDavilaKurban <fdavilakurban@unc.edu.ar>
2024-09-12 18:01:48 +0200

    starting 2pcf

---
+++ cosm_parameters.py
@@ -0,0 +1,14 @@
+# Cosmological Parameter values
+cosm_param = dict(
+    { 
+        "Om": {'fid':0.3175,\
+            'step':0.01},
+        "ns": {'fid':0.9624,\
+            'step':0.02},
+        "h": {'fid':0.6711,\
+            'step':0.02},
+        "Ob2": {'fid':0.049,\
+               'step':0.02
+        }
+    })
+
---
+++ file_parameters.py
@@ -0,0 +1,10 @@
+file_parameters = dict(
+    {   
+        "ns":int(10000),
+        "rbin":int(13),
+        "rmin":3.,
+        "rmax":int(25),
+        "njk":int(10),
+        "space":'zspace',
+        "filedir":'../data/output/'
+    })
