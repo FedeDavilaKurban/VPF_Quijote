76d08bf95b0ee7879bdf93a47e440230a5004a0f
FedeDavilaKurban <fdavilakurban@unc.edu.ar>
2024-09-19 16:43:08 +0200

    fisher matrix 2pcf/vpf plots progress

--- file_parameters.py
+++ file_parameters.py
@@ -1,4 +1,4 @@
-file_parameters = dict(
+file_parameters_vpf = dict(
     {   
         "ns":int(10000),
         "rbin":int(13),
@@ -7,4 +7,15 @@ file_parameters = dict(
         "njk":int(10),
         "space":'zspace',
         "filedir":'../data/output/'
+    })
+
+file_parameters_2pcf = dict(
+    {   
+        "nbins_s":9,
+        "nbins_m":10,
+        "rmin":10,
+        "rmax":150,
+        "space":'zspace',
+        "filedir":'../data/output/2pcf/',
+        "axis":0
     })
